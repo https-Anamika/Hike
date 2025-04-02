576a5ade6d0b027c60f08b89f015aae03f2a009f
https-Anamika <https-Anamika@github.com>
2025-04-03 00:08:57 +0530

    first commit

---
+++ Hike/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  Hike
+//
+//  Created by anamika singh on 03/04/25.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Card()
+}
